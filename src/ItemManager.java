efffa96ab95fc11a8239f044b5915a66a0e9b0f5
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-10 12:06:29 +0100

    risistemata probabilità oggetti

--- src/ItemManager.java
+++ src/ItemManager.java
@@ -16,7 +16,7 @@ public class ItemManager {
 
     public List<Item> generaOggetti(char[][] mappa, int livello) {
         items.clear();
-        int maxOggetti = random.nextInt(2)+10; // Da 0 a 2 oggetti
+        int maxOggetti = random.nextInt(1)+1; // Da 1 a 2 oggetti
         for (int i = 0; i < maxOggetti; i++) {
             while (true) {
                 int row = random.nextInt(mappa.length);
