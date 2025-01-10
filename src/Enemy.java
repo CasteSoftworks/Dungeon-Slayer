c4ef46344aa7ab4d1a0af5fd978eca47f01c9717
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-10 15:29:52 +0100

    aggiunta di Javadoc per le classi Enemy e Item, migliorando la documentazione del codice

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,67 +1,114 @@
 public class Enemy {
-    private int row;
-    private int col;
-    private char tipo; // 'Z' per Zombie, 'S' per Scheletro, V per Vampiro.
+    /** Le coordinate del nemico */
+    private int row, col;
+    /** 
+     * Il tipo di nemico
+     * 
+     * <ul>
+     * <li>'Z' per Zombie</li>
+     * <li>'S' per Scheletro</li>
+     * <li>'V' per Vampiro</li>
+     * </ul>
+     */
+    private char tipo;
+    /** Vita e danni del nemico */
     private int vita;
     private int danni;
 
+    /**
+     * Costruttore di Enemy
+     * 
+     * @param row riga
+     * @param col colonna
+     * @param tipo tipo di nemico
+     * @param vita vita del nemico
+     * @param danni danni del nemico
+     */
     public Enemy(int row, int col, char tipo, int vita, int danni) {
         this.row = row;
         this.col = col;
         this.tipo = tipo;
         this.vita = vita;
         this.danni = danni;
-        //this.sprite = sprite;
     }
 
+    /**
+     * Restituisce la riga del nemico
+     * 
+     * @return la riga
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Imposta la riga del nemico
+     * 
+     * @param row riga
+     */
     public void setRow(int row) {
         this.row = row;
     }
 
+    /**
+     * Restituisce la colonna del nemico
+     * 
+     * @return la colonna
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * Imposta la colonna del nemico
+     * 
+     * @param col colonna
+     */
     public void setCol(int col) {
         this.col = col;
     }
 
+    /**
+     * Restituisce il tipo di nemico
+     * 
+     * @return il tipo
+     */
     public char getTipo() {
         return tipo;
     }
 
+    /**
+     * Recupera la vita del nemico
+     * 
+     * @return vita
+     */
     public int getVita() {
         return vita;
     }
 
-    public void setVita(int vita) {
-        this.vita = vita;
-    }
-
+    /**
+     * Recupera i danni del nemico
+     * 
+     * @return
+     */
     public int getDanni() {
         return danni;
     }
 
-    public void setDanni(int danni) {
-        this.danni = danni;
-    }
-
-    public void setTipo(char tipo) {
-        this.tipo = tipo;
-    }
-
-    /*public BufferedImage getSprite() {
-        return sprite;
-    }*/
-
+    /**
+     * Modifica la riga del nemico
+     * 
+     * @param var variazione della riga
+     */
     public void updateRow(int var) {
         this.row+=var; 
     }
 
+    /**
+     * Modifica la colonna del nemico
+     * 
+     * @param var variazione della colonna
+     */
     public void updateCol(int var) {
         this.col+=var;
     }
--- src/Item.java
+++ src/Item.java
@@ -1,9 +1,19 @@
 public class Item {
-    private int row;
-    private int col;
+    /** Coordinate dell'oggetto */
+    private int row, col;
+    /** Tipo di oggetto */
     private char tipo; // 'A' = Armor, 'W' = Weapon, 'H' = Health
+    /** Valore dell'oggetto */
     private int value;
 
+    /**
+     * Costruttore di Item
+     * 
+     * @param row riga
+     * @param col colonna
+     * @param tipo tipo di oggetto
+     * @param value valore dell'oggetto
+     */
     public Item(int row, int col, char tipo, int value) {
         this.row = row;
         this.col = col;
@@ -11,35 +21,57 @@ public class Item {
         this.value = value;
     }
 
+    /**
+     * Restituisce la riga dell'oggetto
+     * 
+     * @return la riga
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Imposta la riga dell'oggetto
+     * 
+     * @param row riga
+     */
     public void setRow(int row) {
         this.row = row;
     }
 
+    /**
+     * Restituisce la colonna dell'oggetto
+     * 
+     * @return la colonna
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * Imposta la colonna dell'oggetto
+     * 
+     * @param col colonna
+     */
     public void setCol(int col) {
         this.col = col;
     }
 
+    /**
+     * Restituisce il tipo di oggetto
+     * 
+     * @return
+     */
     public char getTipo() {
         return tipo;
     }
 
+    /**
+     * Restituisce il valore dell'oggetto
+     * 
+     * @return
+     */
     public int getValue() {
         return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
-    public void setTipo(char tipo) {
-        this.tipo = tipo;
     }
 }
