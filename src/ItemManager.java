b982202fcc55da1297677b92df10b42c30dc3937
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-10 14:00:32 +0100

    javadoc

--- src/ItemManager.java
+++ src/ItemManager.java
@@ -3,17 +3,35 @@ import java.util.List;
 import java.util.Random;
 
 public class ItemManager {
+    /** Lista di oggetti */
     private final List<Item> items;
+    /** Generatore di numeri casuali */
     private final Random random = new Random();
 
+    /** 
+     * Costruttore di ItemManager
+     */
     public ItemManager() {
         this.items = new ArrayList<>();
     }
     
+    /**
+     * Restituisce la lista di oggetti
+     * 
+     * @return lista di oggetti
+     */
     public List<Item> getItems() {
         return items;
     }
 
+    /**
+     * Genera oggetti casuali sulla mappa
+     * 
+     * @param mappa mappa del livello
+     * @param livello livello corrente
+     * 
+     * @return lista di oggetti generati
+     */
     public List<Item> generaOggetti(char[][] mappa, int livello) {
         items.clear();
         int maxOggetti = random.nextInt(1)+1; // Da 1 a 2 oggetti
@@ -37,6 +55,13 @@ public class ItemManager {
         return new ArrayList<>(items);
     }
 
+    /**
+     * Determina il tipo di oggetto da generare a seconda del livello
+     * 
+     * @param livello livello corrente
+     * 
+     * @return tipo di oggetto
+     */
     private char determinaTipoOggetto(int livello) {
         int probabilità = random.nextInt(100) + 1;
 
@@ -48,19 +73,22 @@ public class ItemManager {
         } else if (livello <= 10) {
             if (probabilità <= 50) return 'H'; // Health 50%
             if (probabilità <= 100) return 'A'; // Armor 50%
-        } else if (livello <= 15) {
-            if (probabilità <= 20) return 'H'; // Health 20%
-            if (probabilità <= 95) return 'A'; // Armor 75%
-            if (probabilità <= 100) return 'W'; // Weapon 5%
         } else {
-            if (probabilità <= 10) return 'H'; // Health 10%
-            if (probabilità <= 80) return 'A'; // Armor 70%
+            if (probabilità <= 40) return 'H'; // Health 40%
+            if (probabilità <= 80) return 'A'; // Armor 40%
             if (probabilità <= 100) return 'W'; // Weapon 20%
-        }
+        } 
 
         return ' ';
     }
 
+    /**
+     * Determina il valore dell'oggetto a seconda del tipo
+     * 
+     * @param tipoOggetto tipo di oggetto
+     * 
+     * @return valore dell'oggetto
+     */
     private int determinaValoreOggetto(char tipoOggetto) {
         switch (tipoOggetto) {
             case 'H':
