9317025e154a0a807f6b4332390e6b71ddc3b808
giacomo.castellana <giacomo.castellana@studenti.unimi.it>
2025-01-11 18:20:14 +0100

    rename di App a DungeonSlayer

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        GameLauncher launcher = new GameLauncher();
-        launcher.launch();
-
-    }
-}
---
+++ src/DungeonSlayer.java
@@ -0,0 +1,7 @@
+public class DungeonSlayer {
+    public static void main(String[] args) throws Exception {
+        GameLauncher launcher = new GameLauncher();
+        launcher.launch();
+
+    }
+}
